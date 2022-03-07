5e436df5b295e360ce842a62c35b20893399d0d7
r_gomes <r.gomes@mega.com.br>
2022-03-07 20:17:59 -0300

    Aula 124 - introdução à enumerações

---
+++ aula enum/Aula enum/course/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course.Entities.Enums
+{
+    enum OrderStatus : int
+        //O :int se trata de uma derivação de tipos, o orderstatus é uma derivação do tipo Int
+    {
+        //se não for informado um número, será considerado sequencial
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+
+    }
+}
--- aula enum/Aula enum/course/Program.cs
+++ aula enum/Aula enum/course/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using course.Entities;
+using course.Entities.Enums;
 
 namespace course
 {
@@ -6,7 +8,21 @@ namespace course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order{
+            Id = 1,
+            Moment = DateTime.Now,
+            Status = OrderStatus.Processing
+            };
+
+            Console.WriteLine(order.ToString());
+            //conversão de enum para string, usando o ToString
+            string vTxt = OrderStatus.Delivered.ToString();
+            Console.WriteLine(vTxt);
+            //Conversão de texto para Enum, utiliza-se o Enum.Parsse<TipoDoEnum>(string):
+            OrderStatus os = Enum.Parse<OrderStatus>("Processing");
+
+            Console.WriteLine(os);
+
         }
     }
 }
